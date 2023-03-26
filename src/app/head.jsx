d55c74450d17c73f59e0231743bd9df88b113a16
notescraze <serenetechqqo24@gmail.com>
2023-03-26 10:19:15 +0800

    install next js

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMnotDB</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="We are the original IMDB website" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
