a109ad03025b45617eac249a87d063072f54026b
notescraze <serenetechqqo24@gmail.com>
2023-03-26 11:57:11 +0800

    tailwind setup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,5 +4,9 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>HOME</h1>;
+  return (
+    <div>
+      <h1 className="text-red-400">hello</h1>HOME
+    </div>
+  );
 }
