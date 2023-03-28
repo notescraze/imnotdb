2144565514543e8b9e98819c8a0beec0b8008491
notescraze <serenetechqqo24@gmail.com>
2023-03-28 13:47:51 +0800

    add card component

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center">
+      <img className="b-96" src="pacmanload.svg" alt="" />
+    </div>
+  );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FiThumbsUp } from "react-icons/fi";
+
+export default function Card({ result }) {
+  const alter = "image not found";
+  return (
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={500}
+          height={300}
+          alt={alter}
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+          placeholder="blur"
+          blurDataURL="/pacmanload.svg"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
+        <div className="p-2">
+          <p className="line-clamp-2 text-md">{result.overview}</p>
+          <h2 className="truncate text-lg font-bold mb-2">
+            {result.title || result.name}
+          </h2>
+          <p className="flex items-center text-sm">
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className="h-5 mr-1 ml-5" />
+            {result.vote_count}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+}
