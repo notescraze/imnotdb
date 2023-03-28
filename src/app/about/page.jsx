fd45c1e5840e7d3aed96d61c2b660056a221d40c
notescraze <serenetechqqo24@gmail.com>
2023-03-28 10:45:41 +0800

    lot of additions and changes

---
+++ src/app/about/page.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2x1 font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+        impedit earum dolorem itaque atque, architecto quae asperiores. Sed
+        dolores, dolor eum quaerat, vero et quo aut soluta quam, doloribus
+        asperiores sint debitis in aliquam! Cumque earum mollitia ad
+        consectetur, esse deserunt commodi repellat debitis. Ut dolores corrupti
+        ratione iste eaque!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
+        voluptatum soluta placeat nihil. Dolorem quasi excepturi animi aut,
+        inventore natus sunt saepe incidunt corrupti illum illo reprehenderit
+        commodi debitis aliquid?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea veritatis
+        qui at alias eos voluptate explicabo nulla adipisci quam eius dolor
+        totam magni excepturi minus deleniti, reiciendis repellendus nam libero
+        perferendis nobis magnam asperiores. Fuga, error illum. Autem nesciunt
+        quas, ipsa officiis esse provident necessitatibus earum vel repellat!
+        Aspernatur maiores dignissimos vitae veniam minus. Ex officiis earum
+        porro molestiae eveniet ipsam minima perspiciatis ullam sit?
+      </p>
+    </div>
+  );
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import Providers from "./Providers";
 
@@ -10,7 +11,7 @@ export default function RootLayout({ children }) {
           {/* header */}
           <Header />
           {/* navbar */}
-
+          <Navbar />
           {/* searchbox */}
 
           {children}
